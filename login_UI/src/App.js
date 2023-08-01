967e007b58f8c8847e9a6c0c8299953973068edc
snimmaReddy <sharaschandrika.27@gmail.com>
2023-08-01 18:24:55 -0400

    Login_Page

--- login_UI/src/App.js
+++ login_UI/src/App.js
@@ -1,7 +1,7 @@
 import { Login } from "./Login";
 function App() {
   return (
-    <div>
+    <div className="h-screen bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-200 ">
       <Login />
     </div>
   );
--- login_UI/src/Login.js
+++ login_UI/src/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PersonIcon from "@mui/icons-material/Person";
 import { Signup } from "./Signup";
 
 export const Login = () => {
@@ -11,32 +12,44 @@ export const Login = () => {
   };
 
   return (
-    <div class="text-center mt-40 ">
-      <h2 class="text-2xl p-4 ">Login Page</h2>
-      <form class="text-2xl p-4 " onSubmit={handleLogin}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button class="text-2xl p-4" type="submit">
-          Login
-        </button>
-      </form>
-      <a href="/Signup">Register</a>
+    <div class="flex items-center justify-center h-screen">
+      <div>
+        <h2 class="text-center text-5xl font-serif pb-16">Login</h2>
+        <form class="" onSubmit={handleLogin}>
+          <div class="text-center font-serif pb-8">
+            <input
+              className="w-[350px] h-12 p-3 bg-gray-200 text-xl focus:bg-white focus:ring focus:ring-blue-300 rounded-md "
+              type="email"
+              value={email}
+              placeholder="Username"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div class="text-center font-serif pb-8 ">
+            <input
+              className="w-[350px] h-12 p-3 bg-gray-200 text-xl focus:bg-white focus:ring focus:ring-blue-300 rounded-md "
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div class="text-center flex justify-between text-lg font-serif pb-8 ">
+            <a href="/Signup">Register</a>
+            <a href="/ForgotPassword">Forgot password</a>
+          </div>
+          <div className="text-center text-2xl font-serif pb-16">
+            <button
+              class="px-8 bg-slate-300 border-transparent border-2 rounded-md hover:text-black hover:border-black hover:bg-purple-300"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
